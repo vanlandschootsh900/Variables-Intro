4af3e4ca17b15daacedac4478a5b6be65e2e7ea4
vanlandschootsh900 <vanlandschootsh900@northwested.org>
2024-09-06 10:33:45 -0400

    Update variables-practice.py

--- variables-practice.py
+++ variables-practice.py
@@ -27,7 +27,7 @@ greeting = 'Good morning'
 # Define a variable called conversion_factor and assign it the value of 2.54 (a float/floating-point number)
 # Define a variable called book_price and assign it a value of 20.99 (a float/floating-point number)
 # Use the print function to print the value of one of three variables you created in Part 2
-
+my_age = 16
 
 
 
